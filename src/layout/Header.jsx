a7f8b7e86b423c4f556ffc1fc3a4f69723e5e497
ANDREY <41635263+FreylGit@users.noreply.github.com>
2022-11-07 04:55:42 +0300

    translation ru

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,17 @@
-
-
-function Header(){
-    return  <nav className="deep-purple darken-2">
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo">React Movies</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#">Sass</a></li>
-
-      </ul>
-    </div>
-  </nav>
+function Header() {
+    return (
+        <nav className="deep-purple darken-2">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">
+                    React Movies
+                </a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li>
+                        <a href="#">Кино и поиск</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
-export {Header}
+export { Header };
